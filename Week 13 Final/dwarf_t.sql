48d22d48306b089193b16a1a502cd7014dd17393
FishTheFiddler <wesspam11@yahoo.com>
2021-12-09 16:22:34 -0800

    Update dwarf_t.sql

--- Week 13 Final/dwarf_t.sql
+++ Week 13 Final/dwarf_t.sql
@@ -1,5 +1,5 @@
 /********************************************************
-*  Name:           base_t.sql
+*  Name:           dwarf_t.sql
 *  Author:         Wes Peterson
 *  Date:           11 Dec 2021
 *  Purpose:        Complete 325 Final lab.
