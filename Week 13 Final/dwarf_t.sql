7e70485aa96cb6bef8d3a5f1c1ea5dedecda71d4
FishTheFiddler <wesspam11@yahoo.com>
2021-12-09 16:14:58 -0800

    Update dwarf_t.sql

--- Week 13 Final/dwarf_t.sql
+++ Week 13 Final/dwarf_t.sql
@@ -32,14 +32,13 @@ CREATE OR REPLACE
         INSTANTIABLE NOT FINAL;
 /
 
--- Describe type object
+-- Describe
 DESC dwarf_t
 
 /****************************************************************
 * Body Code
 *****************************************************************/
-CREATE OR REPLACE TYPE BODY dwarf_t IS
-  /* Implement a default constructor. */  
+CREATE OR REPLACE TYPE BODY dwarf_t IS  
   CONSTRUCTOR FUNCTION dwarf_t
         ( name       VARCHAR2
         , genus      VARCHAR2 DEFAULT 'Dwarves' ) RETURN SELF AS RESULT IS
@@ -51,29 +50,28 @@ CREATE OR REPLACE TYPE BODY dwarf_t IS
     RETURN;
   END dwarf_t;
  
-  /* Override the get_name function. */
   OVERRIDING MEMBER FUNCTION get_name RETURN VARCHAR2 IS
-  BEGIN
-    RETURN self.name;
-  END get_name;
+    BEGIN
+      RETURN self.name;
+    END get_name;
 
-  /* Implement a get_genus function. */
+-- ------------------- FUNCTION -----------------------------------
   MEMBER FUNCTION get_genus RETURN VARCHAR2 IS
-  BEGIN
-    RETURN self.genus;
-  END get_genus;
+    BEGIN
+      RETURN self.genus;
+    END get_genus;
 
-  /* Implement a set_genus procedure. */
+-- ------------------- PROCEDURE -----------------------------------
   MEMBER PROCEDURE set_genus (genus VARCHAR2) IS
-  BEGIN
-    self.genus := genus;
-  END set_genus;
-  
-  /* Implement an overriding to_string function with generalized invocation. */
+    BEGIN
+      self.genus := genus;
+    END set_genus;
+-- -----------------------------------------------------------------
+
   OVERRIDING MEMBER FUNCTION to_string RETURN VARCHAR2 IS
-  BEGIN
-    RETURN (self AS base_t).to_string||'['||self.name||']['||self.genus||']';
-  END to_string;
+    BEGIN
+      RETURN (self AS base_t).to_string||'['||self.name||']['||self.genus||']';
+    END to_string;
 END;
 /
 
